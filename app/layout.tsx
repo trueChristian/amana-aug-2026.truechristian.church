0e96841590ad73627661c6c2b8f446c2e3ce42e7
eWɘyn <5607939+Llewellynvdm@users.noreply.github.com>
2026-08-31 10:55:07 +0200

    Tighten and darken footer links

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&family=Raleway:wght@400&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Raleway:wght@400&display=swap" rel="stylesheet" />
       </head>
       <body>{children}</body>
     </html>
